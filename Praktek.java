90c87b24099a8fc1605d1c1285b7d77eb5b83a6d
AlfinaSora <80097211+AlfinaSora@users.noreply.github.com>
2021-03-11 01:23:01 +0700

    Add files via upload

---
+++ Praktek.java
@@ -0,0 +1,24 @@
+public class Praktek {
+    public static void main(String[] args) {
+        soal1();
+        soal2();
+        soal3();
+        soal4();
+    }
+    private static void soal1() {
+        int result = -5 + 8 * 6;
+        System.out.println(result);
+    }
+    private static void soal2() {
+        int result = (55+9) % 9;
+        System.out.println(result);
+    }
+    private static void soal3() {
+        int result = 20 + -3*5 / 8;
+        System.out.println(result);
+    }
+    private static void soal4() {
+        int result = 5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println(result);
+    }
+}
